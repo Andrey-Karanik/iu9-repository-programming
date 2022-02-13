f0c56d2a11574357650114a59df805656a94543f
Andrey Karanik <99591785+Andrey-Karanik@users.noreply.github.com>
2022-02-13 19:54:44 +0300

    new

---
+++ programming-languages-and-methods/labs/lab-2/Task-3/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Vector3 vector1 = new Vector3(4, 0, 4);
+        Vector3 vector2 = new Vector3(3, -5, 5);
+        System.out.println(vector1);
+        System.out.println(vector2);
+        System.out.println(vector1.multiply(vector2));
+    }
+}
---
+++ programming-languages-and-methods/labs/lab-2/Task-3/Vector3.java
@@ -0,0 +1,47 @@
+public class Vector3 {
+
+    private int x;
+    private int y;
+    private int z;
+
+    public Vector3(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3 multiply(Vector3 vector3) {
+        Vector3 vector = new Vector3(0, 0, 0);
+        vector.setX(y * vector3.getZ() - vector3.getY() * z);
+        vector.setY(-(x * vector3.getZ() - vector3.getX() * z));
+        vector.setZ(x * vector3.getY() - vector3.getX() * y);
+        return vector;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setX(int value) {
+        x = value;
+    }
+    public void setY(int value) {
+        y = value;
+    }
+    public void setZ(int value) {
+        z = value;
+    }
+
+    public String toString() {
+
+        return "{" + "x: " + x + ", " + "y: " + y + ", " + "z: " + z + "}";
+    }
+}
