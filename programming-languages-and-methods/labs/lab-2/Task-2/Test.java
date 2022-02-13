be12abca545005a03bf468b1dab9271868f3c713
Andrey Karanik <99591785+Andrey-Karanik@users.noreply.github.com>
2022-02-13 17:40:51 +0300

    new

---
+++ programming-languages-and-methods/labs/lab-2/Task-2/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        VectorN vector1 = new VectorN(1, 2, 3);
+        VectorN vector2 = new VectorN(5, 9, -5);
+        VectorN vector3 = new VectorN(6, -6, 0, 5);
+        VectorN vector4 = new VectorN(4, 2, 1, 9);
+        System.out.println(vector1.toString());
+        System.out.println(vector2.toString());
+        System.out.println(vector3.toString());
+        System.out.println(vector4.toString());
+        System.out.println(vector1.multiplyByScalar(vector2));
+        System.out.println(vector3.multiplyByScalar(vector4));
+    }
+}
---
+++ programming-languages-and-methods/labs/lab-2/Task-2/VectorN.java
@@ -0,0 +1,42 @@
+public class VectorN {
+
+    private final int[] coordinates;
+
+    public VectorN(int ... x) {
+        coordinates = x;
+    }
+
+    public int multiplyByScalar(VectorN vectorN) {
+        int sum = 0;
+
+        for (int i = 0; i < coordinates.length; i++) {
+            sum += coordinates[i] * vectorN.getX(i);
+        }
+
+        return sum;
+    }
+
+    public int getX(int i) {
+        return coordinates[i];
+    }
+
+    public void setX(int i, int value) {
+        coordinates[i] = value;
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        str.append("{");
+        for (int i = 0; i < coordinates.length; i++) {
+            str.append("x").append(i + 1).append(": ").append(coordinates[i]);
+            if (i == coordinates.length - 1) {
+                break;
+            }
+            str.append(", ");
+        }
+        str.append("}");
+
+        return str.toString();
+    }
+}
