eb6f24ba21e9093ea59fa8470aed9a63c68d5ed4
Andrey Karanik <99591785+Andrey-Karanik@users.noreply.github.com>
2022-02-24 21:52:00 +0300

    lazy

---
+++ programming-languages-and-methods/not-working-code-laziness/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        SetOfIntegers set = new SetOfIntegers(3, 4, 5, 6);
+        for (int a : set) {
+            System.out.println(a);
+        }
+    }
+}
