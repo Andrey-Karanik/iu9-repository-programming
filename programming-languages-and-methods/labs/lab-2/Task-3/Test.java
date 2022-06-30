cbc9d1c73d3752edc10dd8a9f18700fbf7c13d04
Andrey Karanik <99591785+Andrey-Karanik@users.noreply.github.com>
2022-06-30 12:23:17 +0300

    Delete programming-languages-and-methods directory

--- programming-languages-and-methods/labs/lab-2/Task-3/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Vector3 vector1 = new Vector3(4, 0, 4);
-        Vector3 vector2 = new Vector3(3, -5, 5);
-        System.out.println(vector1);
-        System.out.println(vector2);
-        System.out.println(Vector3.getVectorProduct(vector1, vector2));
-    }
-}
--- programming-languages-and-methods/not-working-code-laziness/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        SetOfIntegers set = new SetOfIntegers(3, 4, 5, 6);
-        for (int a : set) {
-            System.out.println(a);
-        }
-    }
-}
